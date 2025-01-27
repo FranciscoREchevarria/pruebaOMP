2abec87cb8860234c38b976cdaa431ea45dce2ee
FranciscoRodriguezE <darkstr.300592@gmail.com>
2025-01-26 19:30:21 -0700

    Add project files.

---
+++ pruebaOMP/pruebaOMP.cpp
@@ -0,0 +1,50 @@
+// pruebaOMP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <omp.h>
+#include <cstdlib>
+
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
+void imprimeArreglo(float *d);
+
+int main()
+{
+	std::cout << "Sumando Arreglos en Paralelo!\n";
+	float a[N], b[N], c[N];
+	int i;
+
+	for(int i = 0; i < N; i++)
+	{
+		a[i] = rand() % 100 + 1;
+		b[i] = rand() % 100 + 1;
+	}
+	int pedazos = chunk;
+
+	#pragma omp parallel for \
+	shared(a, b, c, pedazos) private(i) \
+	schedule(static, pedazos)
+
+	for (i = 0; i < N; i++)
+	{
+		c[i] = a[i] + b[i];
+	}
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+	imprimeArreglo(a);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+	imprimeArreglo(b);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo x: " << std::endl;
+	imprimeArreglo(c);
+}
+
+void imprimeArreglo(float* d)
+{
+	for (int x = 0; x < mostrar; x++)
+	{
+		std::cout << d[x] << " - ";
+	}
+	std::cout << std::endl;
+}
